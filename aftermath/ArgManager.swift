cd567e0be5fce270a73511d2be9b7eb19ac19a5f
Jaron Bradley <jaron.bradley@gmail.com>
2021-11-18 23:03:24 -0500

    Initial commit

---
+++ aftermath/ArgManager.swift
@@ -0,0 +1,54 @@
+//
+//  ArgParser.swift
+//  aftermath
+//
+//  Created by Jaron Bradley on 11/18/21.
+//
+
+import Foundation
+
+
+class ArgManager {
+    let availableArgs = ["--cleanup"]
+    
+    init(suppliedArgs: [String]) {
+        setArgs(suppliedArgs)
+    }
+    
+    func setArgs(_ args:[String]) {
+        for (x,arg) in (args).enumerated() {
+            if x == 0 || !arg.starts(with: "-") {
+                continue
+            } else if arg == "-h" || arg == "-help" {
+                self.printHelp()
+            } else if arg == "--cleanup" {
+                self.cleanup()
+                exit(1)
+            } else {
+                print("Unidentified argument " + arg)
+                exit(1)
+            }
+        }
+    }
+    
+    func cleanup() {
+        let enumerator = FileManager.default.enumerator(atPath: "/tmp")
+        while let element = enumerator?.nextObject() as? String {
+            if element.hasPrefix("Aftermath_") {
+                let dirToRemove = URL(fileURLWithPath: "/tmp/\(element)")
+                do {
+                    try FileManager.default.removeItem(at: dirToRemove)
+                    print("Removed \(dirToRemove.relativePath)")
+                } catch {
+                    print("\(Date().ISO8601Format()) - Error removing \(dirToRemove.relativePath)")
+                    print(error)
+                }
+            }
+        }
+    }
+    
+    func printHelp() {
+        print("--cleanup -> Remove Aftermath Response Folders")
+        exit(1)
+    }
+}
---
+++ aftermath/CaseHandler.swift
@@ -0,0 +1,94 @@
+//
+//  CaseHandler.swift
+//  aftermath
+//
+//  Created by Jaron Bradley on 11/16/21.
+//
+
+import Foundation
+
+class CaseHandler {
+    let hostName = Host.current().localizedName ?? ""
+    let starttime = Date().ISO8601Format()
+    let caseDir: URL
+    let logFile: URL
+    
+    init() {
+        let caseName = "Aftermath_\(hostName)_\(starttime)"
+        self.caseDir = URL(fileURLWithPath: "/tmp/\(caseName)")
+        self.logFile = caseDir.appendingPathComponent("aftermath.log")
+        self.setup()
+    }
+    
+    private func setup() {
+        self.createCaseDir()
+        let _ = self.createNewCaseFile(dirUrl: self.caseDir, filename: "aftermath.log")
+    }
+    
+    private func createCaseDir() {
+        do {
+            try FileManager.default.createDirectory(at: caseDir, withIntermediateDirectories: true, attributes: nil)
+            print("Response directory created at \(caseDir.relativePath)")
+        } catch {
+            print(error)
+        }
+    }
+    
+    func createNewDir(dirName: String) -> URL {
+        let newUrl = self.caseDir.appendingPathComponent(dirName)
+        
+        do {
+            try FileManager.default.createDirectory(at: newUrl, withIntermediateDirectories: true, attributes: nil)
+        } catch {
+            print(error)
+        }
+        
+        return newUrl
+    }
+    
+    func createNewCaseFile(dirUrl: URL, filename: String) -> URL {
+        let newFile = dirUrl.appendingPathComponent(filename)
+        let path = newFile.relativePath
+        if !(FileManager.default.createFile(atPath: path, contents: nil, attributes: nil)) {
+            print("\(Date().ISO8601Format()) - Error creating \(self.logFile)")
+        }
+        
+        return newFile
+    }
+    
+    func addTextToFile(atUrl: URL, text: String) {
+        let textWithNewLine = "\(text)\n"
+        do {
+            let fileHandle = try FileHandle(forWritingTo: atUrl)
+                fileHandle.seekToEndOfFile()
+                fileHandle.write(textWithNewLine.data(using: .utf8)!)
+                fileHandle.closeFile()
+        } catch {
+            print("Error writing to file \(error)")
+        }
+    }
+    
+    func copyFileToCase(fileToCopy: URL, toLocation: URL?) {
+        var to = self.caseDir
+        if let toLocation = toLocation { to = toLocation }
+        
+        let filename = fileToCopy.lastPathComponent
+        let dest = to.appendingPathComponent(filename)
+        
+        do {
+            try FileManager.default.copyItem(at:fileToCopy, to:dest)
+
+        } catch {
+            print("\(Date().ISO8601Format()) - Error copying \(fileToCopy.relativePath) to case directory")
+        }
+        
+    }
+    
+    func log(_ note: String, displayOnly: Bool = false) {
+        let entry = "\(Date().ISO8601Format()) - \(note)"
+        print(entry)
+        if displayOnly == false {
+            addTextToFile(atUrl: self.logFile, text: entry)
+        }
+    }
+}
