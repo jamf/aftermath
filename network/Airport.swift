fcaa3294fffb189eed5fbf9688ddba590992f151
stuartjash <stuart.ashenbrenner@jamf.com>
2021-12-06 18:53:37 -0800

    network recon

--- network/Airport.swift
+++ network/Airport.swift
@@ -2,7 +2,32 @@
 //  Airport.swift
 //  aftermath
 //
-//  Created by Stuart Ashenbrenner on 12/6/21.
 //
 
 import Foundation
+
+class Airport {
+    
+    let caseHandler: CaseHandler
+    let networkDir: URL
+    let writeFile: URL
+    
+    init(caseHandler: CaseHandler, networkDir: URL, writeFile: URL) {
+        self.caseHandler = caseHandler
+        self.networkDir = networkDir
+        self.writeFile = writeFile
+    }
+    
+    func captureAirportPrefs() {
+        let url = URL(fileURLWithPath: "/Library/Preferences/SystemConfiguration/com.apple.airport.preferences.plist")
+        let plistDict = Aftermath.getPlistAsDict(atUrl: url)
+        
+        self.caseHandler.copyFileToCase(fileToCopy: url, toLocation: self.networkDir)
+        self.caseHandler.addTextToFile(atUrl: self.writeFile, text: "\n----- \(url) -----\n\(plistDict)\n")
+    }
+    
+    func run() {
+        self.caseHandler.log("Collecting airport information...")
+        captureAirportPrefs()
+    }
+}
--- network/NetworkModule.swift
+++ network/NetworkModule.swift
@@ -2,7 +2,24 @@
 //  NetworkModule.swift
 //  aftermath
 //
-//  Created by Stuart Ashenbrenner on 12/6/21.
 //
 
 import Foundation
+
+class NetworkModule {
+    
+    let caseHandler: CaseHandler
+    let networkDir: URL
+    let writeFile: URL
+    
+    init(caseHandler: CaseHandler) {
+        self.caseHandler = caseHandler
+        self.networkDir = caseHandler.createNewDir(dirName: "network")
+        self.writeFile = caseHandler.createNewCaseFile(dirUrl: self.networkDir, filename: "network.txt")
+    }
+    
+    func start() {
+        let airport = Airport(caseHandler: caseHandler, networkDir: self.networkDir, writeFile: self.writeFile)
+        airport.run()
+    }
+}
