e88886684bda1c34e8a8498a72eafdda1ecccd53
stuartjash <stuart.ashenbrenner@jamf.com>
2022-06-17 08:29:09 -0700

    fixed case files to use temp directory and copy to end

--- aftermath/CaseFiles.swift
+++ aftermath/CaseFiles.swift
@@ -3,134 +3,6 @@
 //  aftermath
 //
 //  Created by Jaron Bradley on 12/10/21.
-
-
-//import Foundation
-//
-//struct CaseFiles {
-////    static let caseDir = URL(fileURLWithPath: "/tmp/Aftermath_\(Host.current().localizedName ?? "")_\(Date().ISO8601Format())")
-////    static let logFile = caseDir.appendingPathComponent("aftermath.log")
-////
-//    let caseDir: URL
-//    let logFile: URL
-//    static let analysisCaseDir = URL(fileURLWithPath: "/tmp/Aftermath_Analysis_\(Host.current().localizedName ?? "")_\(Date().ISO8601Format())")
-//    static let analysisLogFile = analysisCaseDir.appendingPathComponent("aftermath_analysis.log")
-//
-//    public static var shared = CaseFiles()
-//
-//
-//    init(tempDir: URL) {
-//        self.caseDir = tempDir
-//        self.logFile = tempDir.appendingPathComponent("aftermath.log")
-//    }
-//
-//    func CreateCaseDir() {
-//        do {
-//            try FileManager.default.createDirectory(at: caseDir, withIntermediateDirectories: true, attributes: nil)
-//            print("Aftermath directory created at \(caseDir.relativePath)")
-//        } catch {
-//            print(error)
-//        }
-//    }
-//
-//    // -------------------
-//
-//
-//
-//
-//
-//
-//    // --------------------
-//
-//
-//
-//    static func CreateAnalysisCaseDir() {
-//        do {
-//            try FileManager.default.createDirectory(at: analysisCaseDir, withIntermediateDirectories: true, attributes: nil)
-//            print("Aftermath Analysis directory created at \(analysisCaseDir.relativePath)")
-//        } catch {
-//            print(error)
-//        }
-//    }
-//}
-
-//
-//import Foundation
-//
-//struct CaseFile {
-//    let path: URL
-//
-//    init(path: URL) {
-//        do {
-//            try FileManager.default.createDirectory(at: path, withIntermediateDirectories: true, attributes: nil)
-//        } catch {
-//            // do something
-//        }
-//        self.path = path
-//    }
-//}
-//
-//struct CaseFiles {
-//    static let tmpDir = URL(fileURLWithPath: "/var/log/boop_\(Date().ISO8601Format())")
-////    static let logFile = caseDir.appendingPathComponent("aftermath.log")
-////    static let analysisCaseDir = URL(fileURLWithPath: "/tmp/Aftermath_Analysis_\("")_\(Date().ISO8601Format())")
-////    static let analysisLogFile = analysisCaseDir.appendingPathComponent("aftermath_analysis.log")
-//
-//    public static var shared = CaseFiles()
-//
-//    public let file: CaseFile
-//
-//    init() {
-//        var caseDir: URL
-//        let destinationURL = URL(fileURLWithPath: "/tmp/")
-//
-//        do {
-//            let temporaryDirectoryURL =
-//                try FileManager.default.url(for: .itemReplacementDirectory,
-//                                            in: .userDomainMask,
-//                                            appropriateFor: destinationURL,
-//                                            create: false)
-//            let temporaryFilename = "Aftermath_\(Host.current().localizedName ?? "")_\(Date().ISO8601Format())"
-//
-//            let temporaryFileURL =
-//                temporaryDirectoryURL.appendingPathComponent(temporaryFilename)
-//            tmpDir = temporaryFileURL
-//            print(temporaryFileURL)
-//        } catch {
-//            print(error)
-//        }
-//        self.file = CaseFile(path: caseDir)
-//    }
-//}
-
-//class TempFiles {
-//
-//
-//    func createTempDir() -> URL {
-//            let destinationURL = URL(fileURLWithPath: "/tmp/")
-//
-//                do {
-//                    let temporaryDirectoryURL =
-//                        try FileManager.default.url(for: .itemReplacementDirectory,
-//                                                    in: .userDomainMask,
-//                                                    appropriateFor: destinationURL,
-//                                                    create: false)
-//                    let temporaryFilename = "Aftermath_\(Host.current().localizedName ?? "")_\(Date().ISO8601Format())"
-//
-//                    let temporaryFileURL =
-//                        temporaryDirectoryURL.appendingPathComponent(temporaryFilename)
-//                    print(temporaryFileURL)
-//                    return temporaryFileURL
-//
-//                } catch {
-//                    print(error)
-//                    exit(1)
-//                }
-//
-//    }
-//}
-
-
 import Foundation
 
 struct CaseFiles {
@@ -183,6 +55,16 @@ class TempDirectory {
             location = temporaryFileURL
             return temporaryFileURL
 
+        } catch {
+            print(error)
+            exit(1)
+        }
+    }
+    
+    func moveTempDirectory(location: URL) {
+        print("Moving temp dir from \(location))")
+        do {
+            try FileManager.default.copyItem(at: location, to: URL(fileURLWithPath: "/tmp/\(location.lastPathComponent)"))
         } catch {
             print(error)
             exit(1)
