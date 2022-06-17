2e3ef54fae22d53dd072e23e5f2e106ce3910de7
stuartjash <stuart.ashenbrenner@jamf.com>
2022-06-17 13:45:57 -0700

    updated temp directory creation

--- aftermath/CaseFiles.swift
+++ aftermath/CaseFiles.swift
@@ -5,73 +5,50 @@
 //  Created by Jaron Bradley on 12/10/21.
 
 
+
 import Foundation
 
 struct CaseFiles {
-    public var caseDir:URL
-    public var logFile:URL
-    public var analysisCaseDir: URL
-    public var analysisLogFile: URL
-        
-
-    init() {
-        self.caseDir = location
-        self.logFile = location.appendingPathComponent("aftermath.log")
-        
-        self.analysisCaseDir = URL(fileURLWithPath: "/tmp/Aftermath_Analysis_\(Host.current().localizedName ?? "")_\(Date().ISO8601Format())")
-        self.analysisLogFile = self.analysisCaseDir.appendingPathComponent("aftermath_analysis.log")
+    static let caseDir = FileManager.default.temporaryDirectory.appendingPathComponent("Aftermath_\(Host.current().localizedName ?? "")_\(Date().ISO8601Format())")
+    static let logFile = caseDir.appendingPathComponent("aftermath.log")
+    static let analysisCaseDir = FileManager.default.temporaryDirectory.appendingPathComponent("Aftermath_Analysis_\(Host.current().localizedName ?? "")_\(Date().ISO8601Format())")
+    static let analysisLogFile = analysisCaseDir.appendingPathComponent("aftermath_analysis.log")
+    
+    static func CreateCaseDir() {
+        do {
+            try FileManager.default.createDirectory(at: caseDir, withIntermediateDirectories: true, attributes: nil)
+            print("Aftermath directory created at \(caseDir.relativePath)")
+        } catch {
+            print(error)
+        }
     }
     
-    func CreateAnalysisCaseDir() {
+    static func CreateAnalysisCaseDir() {
         do {
-            try FileManager.default.createDirectory(at: self.analysisCaseDir, withIntermediateDirectories: true, attributes: nil)
+            try FileManager.default.createDirectory(at: analysisCaseDir, withIntermediateDirectories: true, attributes: nil)
             print("Aftermath Analysis directory created at \(analysisCaseDir.relativePath)")
         } catch {
             print(error)
         }
     }
-}
-
-
-class TempDirectory {
     
-    public var location: URL = URL(fileURLWithPath: "")
-    
-    func createTempDirectory() -> URL {
-        let destinationURL = URL(fileURLWithPath: "/tmp/")
-
+    static func MoveCaseDir() {
+        let endURL = URL(fileURLWithPath: "/tmp/\(caseDir.lastPathComponent)")
+        
         do {
-            let temporaryDirectoryURL =
-                try FileManager.default.url(for: .itemReplacementDirectory,
-                                            in: .userDomainMask,
-                                            appropriateFor: destinationURL,
-                                            create: false)
-            let temporaryFilename = "Aftermath_\(Host.current().localizedName ?? "")_\(Date().ISO8601Format())"
-
-            let temporaryFileURL =
-                temporaryDirectoryURL.appendingPathComponent(temporaryFilename)
-            
-            try FileManager.default.createDirectory(at: temporaryFileURL, withIntermediateDirectories: true, attributes: nil)
-            
-            print("Aftermath directory created at \(temporaryFileURL.relativePath)")
-            location = temporaryFileURL
-            return temporaryFileURL
-
+            try FileManager.default.copyItem(at: caseDir, to: endURL)
         } catch {
             print(error)
-            exit(1)
         }
     }
     
-    func moveTempDirectory(location: URL) {
-        let endURL = URL(fileURLWithPath: "/tmp/\(location.lastPathComponent)")
-
-        print("Moving Aftermath directory from \(location.relativePath) to \(endURL.relativePath)")
+    static func MoveAnalysisCaseDir() {
+        let endURL = URL(fileURLWithPath: "/tmp/\(analysisCaseDir.lastPathComponent)")
+        
         do {
-            try FileManager.default.copyItem(at: location, to: endURL)
+            try FileManager.default.copyItem(at: analysisCaseDir, to: endURL)
         } catch {
             print(error)
-            exit(1)
         }
     }
 }
