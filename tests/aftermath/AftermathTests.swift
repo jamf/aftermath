4f535a6088661b446d4bce4c74852a8dbd25e15c
c7bercat <maggie.zirnhelt@jamf.com>
2022-08-26 11:13:41 -0500

    make this first test work

--- tests/aftermath/AftermathTests.swift
+++ tests/aftermath/AftermathTests.swift
@@ -21,49 +21,12 @@ class AftermathTests: XCTestCase {
 
     func testGetPlistAsDict() throws {
         // Given
-        let url = try XCTUnwrap(URL(string: UUID().uuidString))
-        let plist =
-        """
-        gui/501/com.jamf.protect.agent = {
-            active count = 0
-            path = /Library/LaunchAgents/com.jamf.protect.agent.plist
-            state = spawn scheduled
-
-            program = /Applications/JamfProtect.app/Contents/Helpers/JamfProtectAgent.app/Contents/MacOS/JamfProtectAgent
-            inherited environment = {
-                SSH_AUTH_SOCK => /private/tmp/com.apple.launchd.oi4pZOD54K/Listeners
-            }
-
-            default environment = {
-                PATH => /usr/bin:/bin:/usr/sbin:/sbin
-            }
-
-            environment = {
-                XPC_SERVICE_NAME => com.jamf.protect.agent
-            }
-
-            domain = gui/501 [100007]
-            asid = 100007
-            minimum runtime = 60
-            exit timeout = 5
-            runs = 311
-            last exit code = 0
-
-            spawn type = daemon (3)
-            jetsam priority = 4
-            jetsam memory limit (active) = (unlimited)
-            jetsam memory limit (inactive) = (unlimited)
-            jetsamproperties category = daemon
-            jetsam thread limit = 32
-            cpumon = default
-            job state = exited
-
-            properties = keepalive | runatload
-        }
-        """
-        let expectedData = try XCTUnwrap(plist.data(using: .utf8))
+        let path = try XCTUnwrap(Bundle(for: type(of : self)).path(forResource: "dummyPlist.plist",
+                                                                   ofType: nil))
+        let url = try XCTUnwrap(URL(fileURLWithPath: path))
+        let expectedData = try Data(contentsOf: url)
         let expectedPlist = try XCTUnwrap(PropertyListSerialization.propertyList(from: expectedData,
-                                                                                 format: nil) as? [String:Any]).customMirror
+                                                                                 format: nil) as? [String:Any])
 
         let mockFm = MockFileManager()
         Aftermath.fm = mockFm
@@ -71,17 +34,12 @@ class AftermathTests: XCTestCase {
             XCTAssertEqual(path, url.relativePath)
             return true
         }
-        mockFm.stubContentsAtPath = { path in
-            XCTAssertEqual(path, url.relativePath)
-            return expectedData
-        }
-
 
         // When
         let actualPlist = Aftermath.getPlistAsDict(atUrl: url)
 
         // Then
-        XCTAssertEqual(expectedPlist.description, actualPlist.description)
+        XCTAssertEqual(actualPlist.description, expectedPlist.description)
     }
 
     // MARK: dateFromEpochTimestamp
