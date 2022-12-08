4b1448354fd9b4eae28340c50700f92aadd5926c
stuartjash <stuart.ashenbrenner@gmail.com>
2022-12-08 07:35:57 -0800

    gather extended attributes if they exist during metadata collection

---
+++ extensions/URL.swift
@@ -0,0 +1,44 @@
+//
+//  URL.swift
+//  aftermath
+//
+//  Copyright  2022 JAMF Software, LLC
+//
+
+import Foundation
+
+extension URL {
+
+    // Get list of all extended attributes.
+    func listExtendedAttributes() throws -> [String] {
+
+        let list = try self.withUnsafeFileSystemRepresentation { fileSystemPath -> [String] in
+            let length = listxattr(fileSystemPath, nil, 0, 0)
+            guard length >= 0 else { throw URL.posixError(errno) }
+
+            // Create buffer with required size:
+            var namebuf = Array<CChar>(repeating: 0, count: length)
+
+            // Retrieve attribute list:
+            let result = listxattr(fileSystemPath, &namebuf, namebuf.count, 0)
+            guard result >= 0 else { throw URL.posixError(errno) }
+
+            // Extract attribute names:
+            let list = namebuf.split(separator: 0).compactMap {
+                $0.withUnsafeBufferPointer {
+                    $0.withMemoryRebound(to: UInt8.self) {
+                        String(bytes: $0, encoding: .utf8)
+                    }
+                }
+            }
+            return list
+        }
+        return list
+    }
+
+    // Helper function to create an NSError from a Unix errno.
+    private static func posixError(_ err: Int32) -> NSError {
+        return NSError(domain: NSPOSIXErrorDomain, code: Int(err),
+                       userInfo: [NSLocalizedDescriptionKey: String(cString: strerror(err))])
+    }
+}
