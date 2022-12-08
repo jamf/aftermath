7a9bb5abe53029367601f39879fe990ca01f4f8f
stuartjash <stuart.ashenbrenner@gmail.com>
2022-12-08 11:44:47 -0800

    capture the sha256 of binaries being pointed at from LaunchAgents and LaunchDaemons

---
+++ extensions/String.swift
@@ -0,0 +1,17 @@
+//
+//  String.swift
+//  aftermath
+//
+//  Copyright  2022 JAMF Software, LLC
+//
+
+import Foundation
+
+public extension String {
+    func sha256() -> String{
+        if let stringData = self.data(using: String.Encoding.utf8) {
+            return stringData.sha256()
+        }
+        return ""
+    }
+}
