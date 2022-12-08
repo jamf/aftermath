4dd3937cba3fb17867bbcdd3169d2e1130248a62
stuartjash <stuart.ashenbrenner@gmail.com>
2022-12-08 07:34:31 -0800

    collecting the name of the installed config profiles

---
+++ artifacts/ConfigurationProfiles.swift
@@ -0,0 +1,23 @@
+//
+//  ConfigurationProfiles.swift
+//  aftermath
+//
+//  Copyright  2022 JAMF Software, LLC
+//
+
+import Foundation
+
+class ConfigurationProfiles: ArtifactsModule {
+    
+    override func run() {
+        
+        self.log("Writing installed configuration profiles...")
+        
+        let outputFile = self.createNewCaseFile(dirUrl: moduleDirRoot, filename: "config_profiles.txt")
+            
+        let command = "sudo profiles list -all"
+        let output = Aftermath.shell("\(command)")
+        
+        self.addTextToFile(atUrl: outputFile, text: output)
+    }
+}
