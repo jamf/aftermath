b7cdd8fceb50bd8e9baf1716c3030d7721f80592
stuartjash <stuart.ashenbrenner@gmail.com>
2023-06-29 16:36:57 -0700

    added eslogger output for collecting es events

---
+++ endpointSecurity/ESModule.swift
@@ -0,0 +1,24 @@
+//
+//  ESModule.swift
+//  aftermath
+//
+//  Created by Stuart Ashenbrenner on 6/6/23.
+//
+
+import Foundation
+
+import Foundation
+
+class ESModule: AftermathModule {
+    
+    let name = "EndpointSecurity"
+    var dirName = "EndpointSecurity"
+    var description = "A module that performs endpoint security captures"
+    lazy var moduleDirRoot = self.createNewDirInRoot(dirName: dirName)
+    lazy var esFile = self.createNewCaseFile(dirUrl: moduleDirRoot, filename: "es_logs.json")
+    
+    func run() {
+        let esLogs = ESLogs(outputDir: moduleDirRoot, outputFile: esFile)
+        esLogs.run()
+    }
+}
