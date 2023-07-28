2f03d21e76ec46e80ab92103252320eb275db283
stuartjash <stuart.ashenbrenner@gmail.com>
2023-07-28 00:16:09 -0700

    v1.0.6

--- aftermath/Command.swift
+++ aftermath/Command.swift
@@ -30,7 +30,7 @@ class Command {
     static var collectDirs: [String] = []
     static var unifiedLogsFile: String? = nil
     static var esLogs: [String] = ["create", "exec", "mmap"]
-    static let version: String = "1.5.1"
+    static let version: String = "1.6.0"
     
     static func main() {
         setup(with: CommandLine.arguments)
